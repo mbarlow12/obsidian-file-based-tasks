482bd2050cabc7f9fa5ed66531a9a8f45e35a94c
Michael Barlow <m.barlow12@gmail.com>
2021-11-22 20:34:10 -0700

    copy files

---
+++ dist.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require("path");
+const os = require('os');
+
+const config = require('./local.config');
+const pwd = process.env.PWD;
+const files = ['styles.css', 'main.ts', 'manifest.json'];
+
+files.forEach(f => {
+    const fp = path.resolve(pwd, f);
+    fs.copyFile(fp, path.resolve(pwd, 'dist', f), err => {
+        if (err) {
+            console.error(`Failed to copy ${fp}`);
+            process.exit(1);
+        }
+    });
+});
+
+fs.readdir(path.resolve(pwd, 'dist'), (err, files) => {
+    if (err) {
+        console.error(`Failed to read dist`);
+        process.exit(1);
+    }
+    for (const file of files) {
+        const distPath = path.resolve(pwd, 'dist', file);
+        const pluginPath = path.resolve(config.dev.distDir, file);
+        fs.copyFile(distPath, pluginPath, err => {
+            if (err) {
+                console.error(`Failed to copy ${distPath}`);
+                process.exit(1);
+            }
+        });
+    }
+});
