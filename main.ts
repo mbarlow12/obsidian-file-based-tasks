a16ab7d45ceda3588186eae5465d9545fb0ad862
Michael Barlow <m.barlow12@gmail.com>
2021-11-22 20:34:45 -0700

    update index on cache change

--- main.ts
+++ main.ts
@@ -14,8 +14,10 @@ import {TaskIndex} from "./src/TaskIndex";
 import TaskParser from "./src/TaskParser";
 import {FileTaskLine, IAnonymousTask, ITask, Task, TaskLocation} from "./src/Task";
 import {TaskFileManager} from "./src/TaskFileManager";
-import {entries, intersection} from 'lodash';
+import {entries, intersection, isEqual} from 'lodash';
 import {TaskEvents} from "./src/Events/TaskEvents";
+import {FileTaskCache} from "./src/File/types";
+import {getFileTaskCache} from "./src/File";
 
 const DEFAULT_SETTINGS: TaskManagerSettings = {
     taskDirectoryName: 'tasks',
@@ -30,11 +32,14 @@ export default class ObsidianTaskManager extends Plugin {
     settings: TaskManagerSettings;
     index: TaskIndex;
     taskFileManager: TaskFileManager;
+    // filepath: task cache
+    fileTaskCaches: Record<string, FileTaskCache>;
     private initialized = false;
     private taskEvents: TaskEvents;
 
     constructor(app: App, manifest: PluginManifest) {
         super(app, manifest);
+        this.fileTaskCaches = {};
     }
 
     async onload() {
@@ -43,7 +48,7 @@ export default class ObsidianTaskManager extends Plugin {
             if (!this.initialized) {
                 await this.loadSettings();
                 this.taskFileManager = new TaskFileManager(this.app.vault, this.app.metadataCache, this.settings.taskDirectoryName)
-                await this.processTasksDirectory();
+                await this.processVault();
                 this.taskEvents = new TaskEvents(this.app.workspace);
                 await this.registerEvents();
                 this.initialized = true;
@@ -65,21 +70,22 @@ export default class ObsidianTaskManager extends Plugin {
 
     registerEvents() {
         this.registerEvent(this.app.vault.on('create', this.handleFileCreated.bind(this)));
-        this.registerEvent(this.app.vault.on('modify', this.handleFileModified.bind(this)));
+        // this.registerEvent(this.app.vault.on('modify', this.handleCacheChanged.bind(this)));
         this.registerEvent(this.app.vault.on('delete', this.handleFileDeleted.bind(this)));
         this.registerEvent(this.app.vault.on('rename', this.handleFileRenamed.bind(this)));
+        this.registerEvent(this.app.metadataCache.on('changed', this.handleCacheChanged.bind(this)))
 
         this.registerEvent(this.app.workspace.on('file-open', file => {
             console.log('FILE-OPEN', file.path);
         }));
 
-        this.registerEvent(this.taskEvents.registerRequestIndexUpdateHandler(({filePath, taskRecord}) => {
-            this.index.handleIndexUpdateRequest(filePath, taskRecord);
-        }));
+        this.registerEvent(
+            this.taskEvents.registerRequestIndexUpdateHandler(
+                ({filePath, taskRecord}) => {
+                    this.index.handleIndexUpdateRequest(filePath, taskRecord);
+                }
+            ));
 
-        this.registerEvent(this.app.metadataCache.on('changed', (arg) => {
-            console.log('METADATA CACHE CHANGED', arg.path);
-        }));
         this.registerEvent(this.app.metadataCache.on('resolve', (arg) => {
             console.log('METADATA CACHE RESOLVE', arg.path);
         }));
@@ -105,7 +111,7 @@ export default class ObsidianTaskManager extends Plugin {
      * @param abstractFile
      * @private
      */
-    private handleFileModified(abstractFile: TAbstractFile) {
+    private handleCacheChanged(abstractFile: TAbstractFile) {
         if (this.app.workspace.activeLeaf.view instanceof MarkdownView) {
             if (this.app.workspace.activeLeaf.view.file.path !== abstractFile.path) {
                 // automated write from the file manager
@@ -114,25 +120,32 @@ export default class ObsidianTaskManager extends Plugin {
         }
 
         if (abstractFile instanceof TFile) {
-            this.taskFileManager.parseTasksFromFile(abstractFile)
-                .then(tasksData => {
-                    const [t, rec] = tasksData;
-                    const tasks: ITask[] = []
-                    if (t)
-                        tasks.push(Task.fromITask(t));
-                    else if (rec) {
-                        const keys = Object.keys(rec);
-                        for (let i = 0; i < keys.length; i++) {
-                            const anonTask = rec[i];
-                            const task = new Task(anonTask.name, anonTask.status);
-                            task.addLocation({filePath: abstractFile.path, line: i});
-                            tasks.push(task);
-                        }
-                    }
+            const fileTaskCache = this.fileTaskCaches[abstractFile.path];
+            const fileMetadataCache = this.app.metadataCache.getFileCache(abstractFile);
+            this.app.vault.read(abstractFile)
+                .then(contents => getFileTaskCache(fileMetadataCache, contents))
+                .then(newTaskCache => {
+                    this.taskFileManager.parseTasksFromFile(abstractFile)
+                        .then(tasksData => {
+                            const [t, rec] = tasksData;
+                            const tasks: ITask[] = []
+                            if (t)
+                                tasks.push(Task.fromITask(t));
+                            else if (rec) {
+                                const keys = Object.keys(rec);
+                                for (let i = 0; i < keys.length; i++) {
+                                    const anonTask = rec[Number.parseInt(keys[i])];
+                                    const task = new Task(anonTask.name, anonTask.status);
+                                    task.addLocation({filePath: abstractFile.path, line: i});
+                                    tasks.push(task);
+                                }
+                            }
 
-                    if (tasks.length)
-                        this.taskEvents.triggerRequestIndexUpdate(abstractFile.path, tasks);
-                });
+                            if (tasks.length) {
+                                this.taskEvents.triggerRequestIndexUpdate(abstractFile.path, tasks);
+                            }
+                        });
+                })
         }
     }
 
@@ -142,13 +155,6 @@ export default class ObsidianTaskManager extends Plugin {
 
     private handleFileRenamed(abstractFile: TAbstractFile) {
 
-    }
-
-    private indexTodosFromFile(tFile: TFile) {
-        this.app.vault.cachedRead(tFile)
-            .then(TaskParser.parseLines)
-            .then(taskLines => this.processFileTasks(taskLines, tFile))
-            .catch(console.log);
     }
 
     public get tasksDirectory(): TFolder | null {
@@ -172,7 +178,7 @@ export default class ObsidianTaskManager extends Plugin {
         for (const tFile of tasksFolder.children) {
             tasks.push(this.taskFileManager.getTaskFromTaskFile(tFile as TFile, index));
         }
-        Promise.all(tasks)
+        return Promise.all(tasks)
             .then(all => {
                 this.index = new TaskIndex(all);
             });
@@ -180,96 +186,32 @@ export default class ObsidianTaskManager extends Plugin {
 
     private async processVault() {
         await this.processTasksDirectory();
-        for (const file of this.app.vault.getMarkdownFiles().filter(f => !f.path.includes(this.settings.taskDirectoryName))) {
-            const fileRecord = await this.taskFileManager.getTasksFromFile(file);
-            const newTasks: ITask[] = [];
-            for (let lineNumber in fileRecord) {
-                const anonTask = fileRecord[Number.parseInt(lineNumber)];
-                newTasks.push(Task.fromAnonymousTask(anonTask));
-            }
-            if (newTasks.length > 0) {
-                this.taskEvents.triggerRequestIndexUpdate(file.path, newTasks);
-            }
-        }
-    }
-
-    private buildTaskIndex() {
-        const index = new TaskIndex();
-        const vault = this.app.vault;
-        const cache = this.app.metadataCache;
-        const files = vault.getMarkdownFiles();
-        for (const tFile of files) {
-            vault.cachedRead(tFile)
-                .then(contents => {
-                    const {frontmatter, listItems} = cache.getFileCache(tFile);
-                })
-        }
-    }
-
-    /**
-     *
-     * @param bTasks
-     * @param tFile
-     * @private
-     */
-    private processFileTasks(bTasks: FileTaskLine[], tFile: TFile) {
-        for (const [lineNo, {name, status}] of bTasks) {
-            const primaryTask = this.index.getTaskByName(name);
-            if (!primaryTask) {
-                // create new task
-            } else {
-                // add location to task
-            }
+        for (const file of this.app.vault.getMarkdownFiles()) {
+            if (file.path.includes(this.settings.taskDirectoryName))
+                continue;
+            await this.app.vault.read(file).then(fileContents => {
+                const fileCache = this.app.metadataCache.getFileCache(file);
+                this.fileTaskCaches[file.path] = getFileTaskCache(fileCache, fileContents);
+                for (const [line, task] of TaskParser.parseLines(fileContents)) {
+                    const loc: TaskLocation = {filePath: file.path, line};
+                    if (!this.index.taskExists(task.name)) {
+                        // this is a problem
+                        console.warn(`Task "${task.name}" in ${file.path} at ${line} not in index.`)
+                        const iT = Task.fromAnonymousTask(task);
+                        iT.locations = [...(iT.locations || []), loc];
+                        this.index.addTask(iT);
+                    } else {
+                        // assume that other metadata is accurate from the tasks directory processing
+                        const existing = this.index.getTaskByName(task.name);
+                        existing.status = task.status;
+                        // update locations
+                        const foundLocI = existing.locations.findIndex(eLoc => isEqual(eLoc, loc));
+                        if (foundLocI === -1)
+                            existing.locations.push({filePath: file.path, line});
+                    }
+                }
+            });
+            // TODO: change to event trigger to update from file
         }
     }
 }
-
-function handlKeyup(event: KeyboardEvent) {
-    console.log('keyupping!');
-}
-
-/*
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: TaskManagerPlugin;
-
-	constructor(app: App, plugin: TaskManagerPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}*/
