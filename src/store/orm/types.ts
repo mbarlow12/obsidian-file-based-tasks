ec111ac572a103da9510f5a46e43effd8f0365e3
Michael Barlow <m.barlow12@gmail.com>
2022-07-24 13:24:40 -0600

    comments messing with table row typing

--- src/store/orm/types.ts
+++ src/store/orm/types.ts
@@ -41,3 +41,61 @@ export type FileITaskInstanceRecord = Record<number, ITaskInstance>;
 
 // export type Row<T> = [ ...(keyof T) ]
 
+// type UnionToIntersection<U> = (
+//   U extends never ? never : (arg: U) => never
+// ) extends (arg: infer I) => void
+//   ? I
+//   : never;
+//
+// export type UnionToTuple<T> = UnionToIntersection<
+//   T extends never ? never : (t: T) => T
+// > extends (_: never) => infer W
+//   ? [...UnionToTuple<Exclude<T, W>>, W]
+//   : [];
+//
+// export type ValueUnionToTuple<T, K extends keyof T> = UnionToIntersection<
+//     K extends never ? never : (t: K) => K
+// > extends (_: never) => infer W
+//     ? [ ...ValueUnionToTuple<T, Exclude<K, W>>, T[W]]
+//     : [];
+//
+// export interface Simple {
+//     name: string;
+//     id: number;
+//     created: number;
+//     due: number;
+//     complete: boolean;
+//     completed: number;
+//     content: string;
+// }
+// const vals = <T>(obj: T, fields: UnionToTuple<keyof T>) => fields.map((f: keyof T) => obj[f]);
+// const s: Simple = {
+//     name: 'name',
+//     id: 1,
+//     created: 2,
+//     due: 3,
+//     complete: false,
+//     completed: 4,
+//     content: 'content',
+// };
+// type KS = keyof Simple;
+// const c: UnionToTuple<KS> = ['name', 'content', 'created', 'id', 'complete', 'completed', 'due'];
+// const f = vals(s, c);
+
+// type ObjUnionToTuple<T> = UnionToIntersection<keyof T> extends (_: never ) => infer W
+//     ? [...ObjUnionToTuple<Omit<T, Exclude<keyof T, W>>>, T[W]]
+//                              : []
+//
+// type SimpleKeys = UnionToTuple<KS>;
+// const c: UnionToTuple<Exclude<KS, 'due'>> = ['name', 'content', 'created', 'id', 'complete', 'completed'];
+// const d: ObjUnionToTuple<Simple> = ['hello', '', new Date().getTime(), 12, true, 11293];
+// type Val<T, K extends keyof T> = []
+
+// const fNames: U2T<keyof ITaskInstance> = [
+//     'name', 'dueDate', 'id', 'complete', 'tags', 'completed', 'rawText', 'filePath', 'line', 'parentLine',
+//     'parentInstance', 'childLines', 'instanceChildren', 'links'
+// ]
+
+
+
+
